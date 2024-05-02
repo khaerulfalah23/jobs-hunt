eef02d535c77a53224caccdac4edc46162a332a4
khaerulfalah23 <khaerulfalah23@gmail.com>
2024-05-01 12:01:03 -1200

    fea: slicing signup page

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -3,3 +3,13 @@ import { z } from 'zod';
 export const formFilterSchema = z.object({
   categories: z.array(z.string()),
 });
+
+export const formSignUpSchema = z.object({
+	email: z
+		.string({ required_error: "Email is requied" })
+		.email({ message: "Email is not valid" }),
+	password: z.string({ required_error: "Password is required" }),
+	name: z
+		.string({ required_error: "Name is required" })
+		.min(3, { message: "Name should have minimal 3 characters" }),
+});
