5c74ae6b94e44de9b28950a745d32b31d92fc88d
khaerulfalah23 <khaerulfalah23@gmail.com>
2024-05-02 19:47:57 -1200

    feat: integration latest jobs

--- src/components/organisms/LatestJobs/index.tsx
+++ src/components/organisms/LatestJobs/index.tsx
@@ -3,23 +3,22 @@
 import TitleSection from '@/components/atoms/TitleSection';
 import { FC } from 'react';
 import JobItem from './JobItem';
+import useFeaturedJobs from '@/hooks/useFeaturedJobs';
+import { JobType } from '@/types';
 
 interface LatestJobsProps {}
 
 const LatestJobs: FC<LatestJobsProps> = ({}) => {
+  const { jobs, isLoading, error } = useFeaturedJobs();
+
   return (
     <div className="py-16 mt-32 mb-10 relative">
       <TitleSection word1="Latest" word2="jobs open" />
 
       <div className="mt-12 grid grid-cols-3 gap-8">
-        <JobItem
-          image="/images/hero.png"
-          jobType="Full time"
-          location="Jakarta, Indonesia"
-          name="UI/UX Designer"
-          type="UI/UX Designer"
-          skills={['AdobeXD', 'AdobeXD', 'AdobeXD', 'AdobeXD', 'AdobeXD']}
-        />
+        {jobs.map((item: JobType) => (
+          <JobItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
