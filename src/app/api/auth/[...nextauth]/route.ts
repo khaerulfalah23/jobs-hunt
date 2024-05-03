cebf70a6278d942d1da50ebdd9938150f81a5865
khaerulfalah23 <khaerulfalah23@gmail.com>
2024-05-02 21:18:59 -1200

    fix: login page(next auth)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import { authOptions } from '@/lib/utils';
 import NextAuth from 'next-auth/next';
+import authOptions from './options';
 
 const handler = NextAuth(authOptions);
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
-import { NextAuthOptions } from 'next-auth';
 import { CompanyType, JobType, categoryJobType, optionType } from '@/types';
 import { twMerge } from 'tailwind-merge';
-import CredentialsProvider from 'next-auth/providers/credentials';
 import bcrypt from 'bcryptjs';
-import prisma from '../../lib/prisma';
 import { supabasePublicUrl } from './supabase';
 import dayjs from 'dayjs';
 
@@ -52,63 +49,6 @@ export const parsingCategories = (
   }
 
   return [];
-};
-
-export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: {
-          label: 'Email',
-          type: 'text',
-        },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
-        const user = await prisma.user.findFirst({
-          where: {
-            email: credentials?.email,
-          },
-        });
-
-        if (!user) {
-          return null;
-        }
-
-        const isMatch = await comparePassword(
-          credentials?.password!!,
-          user.password
-        );
-
-        if (isMatch) {
-          return user;
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: '/signin',
-    error: '/auth/error',
-    newUser: '/signup',
-  },
-  callbacks: {
-    jwt({ token, account, user }) {
-      if (account) {
-        token.id = user.id;
-      }
-
-      return token;
-    },
-    async session({ session, token }) {
-      session.user.id = token.id;
-
-      return session;
-    },
-  },
 };
 
 export const parsingCategoriesToOptions = (
